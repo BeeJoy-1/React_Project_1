073d3f59574b3afc413b3334e90c998b1024bbe7
BeeJoy-1 <beejoy3000@gmail.com>
2024-04-22 12:22:20 +0600

    did the pricing part

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,8 +1,127 @@
 import React from 'react'
+import { BsCaretRightFill } from "react-icons/bs";
+{/* <BsCaretRightFill /> */}
+
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <>
+      <div className='pt-[220px] pb-[128px]'>
+        <div className='w-[1280px] mx-auto'>
+          <div>
+              <h2 className='text-[40px] font-bold text-center pb-[16px]'>Our Pricing Plans</h2>
+              <p className='w-[481px] mx-auto'>When you’re ready to go beyond prototyping in Figma, Webflow is ready to help you bring your designs to life — without coding them.</p>
+              <div className='flex justify-between items-center'>
+                <div className='pt-[64px] group'>
+                  <div className='w-[405px] py-[64px] pl-[48px] pr-[50px] bg-[#F4F6FC] rounded-[12px] group-hover:bg-[#1C1E53] transition-all delay-0'>
+                    <div className='flex gap-x-[10px] items-center pb-[20px]'>
+                      <span className='text-[35px] font-bold text-[#282938] group-hover:text-white transition-all delay-0'>$299</span>
+                      <h2 className='text-[#2405F2] font-medium group-hover:text-[#FCD980] transition-all delay-0'>Per Design</h2>
+                    </div>
+                    <h2 className='text-[25px] font-medium pb-[8px] group-hover:text-white transition-all delay-0'>Landing Page </h2>
+                    <p className='opacity-60 font-medium pb-[78px] group-hover:text-white transition-all delay-0'>When you’re ready to go beyond prototyping in Figma, </p>
+                    <div className='pb-[80px]'>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>All limited links</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Own analytics platform</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Chat support</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#a5a4a4]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-[#a5a4a4] transition-all delay-0'>Optimize hashtags</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px] text-[#a5a4a4]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px]  text-[#282938] group-hover:text-[#a5a4a4] transition-all delay-0'>Unlimited users</h3>
+                      </div>
+                    </div>
+                    <div className='text-center'>
+                      <button className='py-[16px] px-[78px] bg-[#282938] text-white font-medium rounded-[41px] group-hover:text-black group-hover:bg-[#FCD980] transition-all delay-0'>Contact Us</button>
+                    </div>
+                  </div>
+                </div>
+                <div className='pt-[64px] group'>
+                  <div className='w-[405px] py-[64px] pl-[48px] pr-[50px] bg-[#F4F6FC] rounded-[12px] group-hover:bg-[#1C1E53] transition-all delay-0'>
+                    <div className='flex gap-x-[10px] items-center pb-[20px]'>
+                      <span className='text-[35px] font-bold text-[#282938] group-hover:text-white transition-all delay-0'>$299</span>
+                      <h2 className='text-[#2405F2] font-medium group-hover:text-[#FCD980] transition-all delay-0'>Multi Design</h2>
+                    </div>
+                    <h2 className='text-[25px] font-medium pb-[8px] group-hover:text-white transition-all delay-0'>Website Page  </h2>
+                    <p className='opacity-60 font-medium pb-[78px] group-hover:text-white transition-all delay-0'>When you’re ready to go beyond prototyping in Figma, </p>
+                    <div className='pb-[80px]'>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>All limited links</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Own analytics platform</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Chat support</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Optimize hashtags</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px] text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px]  text-[#282938] group-hover:text-white transition-all delay-0'>Unlimited users</h3>
+                      </div>
+                    </div>
+                    <div className='text-center'>
+                      <button className='py-[16px] px-[78px] bg-[#282938] text-white font-medium rounded-[41px] group-hover:text-black group-hover:bg-[#FCD980] transition-all delay-0'>Contact Us</button>
+                    </div>
+                  </div>
+                </div>
+                <div className='pt-[64px] group'>
+                  <div className='w-[405px] py-[64px] pl-[48px] pr-[50px] bg-[#F4F6FC] rounded-[12px] group-hover:bg-[#1C1E53] transition-all delay-0'>
+                    <div className='flex gap-x-[10px] items-center pb-[20px]'>
+                      <span className='text-[35px] font-bold text-[#282938] group-hover:text-white transition-all delay-0'>$499+</span>
+                      <h2 className='text-[#2405F2] font-medium group-hover:text-[#FCD980] transition-all delay-0'>Per Design</h2>
+                    </div>
+                    <h2 className='text-[25px] font-medium pb-[8px] group-hover:text-white transition-all delay-0'>Complex Project</h2>
+                    <p className='opacity-60 font-medium pb-[78px] group-hover:text-white transition-all delay-0'>When you’re ready to go beyond prototyping in Figma, </p>
+                    <div className='pb-[80px]'>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>All limited links</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Own analytics platform</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Chat support</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px]  text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px] text-[#282938] group-hover:text-white transition-all delay-0'>Optimize hashtags</h3>
+                      </div>
+                      <div className='flex items-center gap-x-[12px] pb-[20px]'>
+                        <span className='text-[25px] text-[#97DAAD]'><BsCaretRightFill /></span>
+                        <h3 className='text-[19px]  text-[#282938] group-hover:text-white transition-all delay-0'>Unlimited users</h3>
+                      </div>
+                    </div>
+                    <div className='text-center'>
+                      <button className='py-[16px] px-[78px] bg-[#282938] text-white font-medium rounded-[41px] group-hover:text-black group-hover:bg-[#FCD980] transition-all delay-0'>Contact Us</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
